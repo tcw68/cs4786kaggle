5dc671fb580280de6eba215f0775884d957ed4c8
Logan Allen <lga24@cornell.edu>
2017-10-17 17:33:56 -0400

    Updates to code

--- kaggleCode.py
+++ kaggleCode.py
@@ -7,46 +7,59 @@ def unique_rows(M):
 	unique_a = np.unique(a.view([('', a.dtype)]*a.shape[1]))
 	return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
 
-## Returns 6000x6000 adjacency matrix
-def createAdjacencyMatrix(graphFile):
-	matrix = np.zeros((6000, 6000), dtype='int32')
-
-	for line in graphFile:
-		(node1, node2) = line.split(',')
-		matrix[node1-1, node2-1] = 1
-		matrix[node2-1, node1-1] = 1
-
-	graphFile.close()
-	return matrix
 
 def findD():
 	return
 
+
 def findL():
 	return
+
 
 def runKMeans():
 	return
 
-# Returns a list of nodes similar (through transitivity) known numbered-nodes
-def findVisited(visited, M):
-	visitNext = deque()
-	for i in visited:
-		visitNext.append(i)
 
-	while len(visitNext) is not 0:
-		element = visitNext.popLeft()
-		print "element", element
+## Returns 6000x6000 adjacency matrix
+def createAdjacencyMatrix(graphFile):
+	print "Creating adjacency matrix..."
+	N = 6000
+	matrix = [[0 for i in range(N)] for j in range(N)]
 
-		for j in range(M.shape[1]):
-			if M[element-1, j] == 1 and j not in visited:
-				visited.append(j)
-				visitNext.append(j)
+	print "Filling in matrix..."
+	for line in graphFile:
+		(node1, node2) = [int(x) for x in line.split(',')]
+		matrix[node1-1][node2-1] = 1
+		matrix[node2-1][node1-1] = 1
 
-		print "length", len(visitNext)
+	graphFile.close()
+	print "Done creating matrix"
+	return matrix
 
-	return visited
 
+# Returns the list of nodes most similar to a specific number
+def similaritySearch(num, seedNodes, M):
+	print "Similarity search for number", num, "..."
+	similarNodes = []
+	similarityRatio = 0.65
+
+	while similarityRatio <= 1.0:
+		for node in range(len(M)):
+			numSimilar = 0
+			for seed in seedNodes:
+				if node == seed:
+					continue
+				if M[node][seed] == 1:
+					numSimilar += 1
+			if (numSimilar/float(len(seedNodes))) >= similarityRatio:
+				similarNodes.append(node)
+		seedNodes += similarNodes
+		similarityRatio += 0.1
+
+	return similarNodes
+
+
+# Run the program
 def run():
 	print "Loading files..."
 	featuresVectorFile = open('Extracted_features.csv', 'r');
@@ -56,31 +69,23 @@ def run():
 
 	M = createAdjacencyMatrix(graphFile);
 
+	# Initialize numbered lists with key=num and value=[node]
+	numberedNodes = {}
+	for i in range(10):
+		numberedNodes[i] = []
 
-	np.set_printoptions(threshold='nan')
-	print "starting features..."
-	featuresVector = np.genfromtxt('Extracted_features.csv', delimiter=',')
-	print "done features"
-	print "starting seeds..."
-	seedPoints = np.genfromtxt('Seed.csv', delimiter=',', dtype='int32')
-	print "end seeds"
+	# Add seed nodes to numbered lists
+	for line in seedFile:
+		(node, num) = [int(x) for x in line.split(',')]
+		numberedNodes[num].append(node)
 
-	M = createAdjacencyMatrix()
+	totalNodesLabeled = 0
+	for num in numberedNodes:
+		similarNodes = similaritySearch(num, numberedNodes[num], M)
+		print "Length of nodes:", len(similarNodes)
+		totalNodesLabeled += len(similarNodes)
+		numberedNodes[num] = similarNodes
+	print "Total nodes labeled:",totalNodesLabeled
 
-	listofVisited = [[], [], [], [], [], [], [], [], [], []] # 0-9
-	finalVisited = []
-
-	for i in range(seedPoints.shape[0]):
-		node = seedPoints[i, 0]
-		digit = seedPoints[i, 1]
-
-		listofVisited[digit].append(node)
-
-	print "starting findVisited"
-
-	for i in range(len(listofVisited)):
-		v = findVisited(listofVisited[i], M)
-		print v
-		finalVisited.append(v)
 
 run()
