f779ced0abbacb0287ac15d063741173fa3f7f46
thomas wong <thomascwong1996@gmail.com>
2017-10-18 01:57:03 -0400

    fixed tiny issues

--- spectral.py
+++ spectral.py
@@ -20,30 +20,37 @@ def numpyMatrix(graphFile):
 
 
 def spectralClustering(M, K):
+	print "Finding normalized D"
 	normD = np.zeros((6000, 6000))
 
 	for i in range(M.shape[0]):
 		normD[i,i] = (float)(1/math.sqrt(sum(M[i,])))
+	print "Done finding normalized D"
 
-	normL = np.subtract(np.identity(6000), np.dot(np.dot(normD, M), normD)
-	evalues, _, vr = LA.eig(normL)
+	print "Finding normalized L"
+	normL = np.subtract(np.identity(6000), np.dot(np.dot(normD, M), normD))
+	print "Done finding normalized L"
+	print "Finding eigenvalues and eigenvectors"
+	evalues, vr = LA.eig(normL)
+	print "Done finding eigenvalues and eigenvectors"
+	
+	print "Finding the smallest K eigenvectors"
 	vectors = []	
-
-
 	order = np.argsort(evalues)
 	for i in range(K):
 		vectors.append(vr[order[i]])
 
 	np_vectors = np.array(vectors)
+	print "Done finding the smallest K eigenvectors"
 
+
+	print "Performed K-Means Clustering"
 	codebook, _ = kmeans(np_vectors, 10)
 	code, _ = vq(vectors, codebook)
-
-
 	clusters = [[],[],[],[],[],[],[],[],[],[]]
 	for i in range(code.shape[0]):
 		clusters[code[i]].append()
-
+	print "Done performing K-Means Clustering"
 
 
 
@@ -58,7 +65,7 @@ def run():
 
 	M = numpyMatrix(graphFile)
 
-	spectralClustering(M)
+	spectralClustering(M, 1000)
 
 
 run()
