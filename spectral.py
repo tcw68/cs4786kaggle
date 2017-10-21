b9fe4564a3aaa569d0d919020f312a3f504bd772
thomas wong <thomascwong1996@gmail.com>
2017-10-20 23:01:22 -0400

    attempt at spectral clustering

--- spectral.py
+++ spectral.py
@@ -3,6 +3,7 @@ from scipy import linalg as LA
 from scipy.cluster.vq import kmeans, vq
 from collections import deque
 import math
+from sklearn import cluster
 
 def numpyMatrix(graphFile):
 	print "Creating numpy adjacency matrix..."
@@ -19,38 +20,34 @@ def numpyMatrix(graphFile):
 	return M
 
 
-def spectralClustering(M, K):
-	print "Finding normalized D"
-	normD = np.zeros((6000, 6000))
+# def spectralClustering(M, K):
+# 	print "Finding normalized D"
+# 	normD = np.zeros((6000, 6000))
 
-	for i in range(M.shape[0]):
-		normD[i,i] = (float)(1/math.sqrt(sum(M[i,])))
-	print "Done finding normalized D"
+# 	for i in range(M.shape[0]):
+# 		normD[i,i] = (float)(1/math.sqrt(sum(M[i,])))
+# 	print "Done finding normalized D"
 
-	print "Finding normalized L"
-	normL = np.subtract(np.identity(6000), np.dot(np.dot(normD, M), normD))
-	print "Done finding normalized L"
-	print "Finding eigenvalues and eigenvectors"
-	evalues, vr = LA.eig(normL)
-	print "Done finding eigenvalues and eigenvectors"
+# 	print "Finding normalized L"
+# 	normL = np.subtract(np.identity(6000), np.dot(np.dot(normD, M), normD))
+# 	print "Done finding normalized L"
+
+# 	print "Finding eigenvalues and eigenvectors"
+# 	evalues, vr = LA.eig(normL)
+# 	print "Done finding eigenvalues and eigenvectors"
 	
-	print "Finding the smallest K eigenvectors"
-	vectors = []	
-	order = np.argsort(evalues)
-	for i in range(K):
-		vectors.append(vr[order[i]])
+# 	print "Finding the smallest K eigenvectors"
+# 	np_vectors = np.array(vr[:K])
+#	np_vectors = np_vectors.T
+# 	print "Done finding the smallest K eigenvectors"
 
-	np_vectors = np.array(vectors)
-	print "Done finding the smallest K eigenvectors"
-
-
-	print "Performed K-Means Clustering"
-	codebook, _ = kmeans(np_vectors, 10)
-	code, _ = vq(vectors, codebook)
-	clusters = [[],[],[],[],[],[],[],[],[],[]]
-	for i in range(code.shape[0]):
-		clusters[code[i]].append()
-	print "Done performing K-Means Clustering"
+# 	print "Performing K-Means Clustering"
+# 	codebook, _ = kmeans(np_vectors, 10)
+# 	code, _ = vq(vectors, codebook)
+# 	clusters = [[],[],[],[],[],[],[],[],[],[]]
+# 	for i in range(code.shape[0]):
+# 		clusters[code[i]].append()
+# 	print "Done performing K-Means Clustering"
 
 
 
@@ -65,7 +62,18 @@ def run():
 
 	M = numpyMatrix(graphFile)
 
-	spectralClustering(M, 1000)
+	# spectralClustering(M, 1000)
+	np.set_printoptions(threshold=np.nan)
+	features = np.genfromtxt('Extracted_features.csv', delimiter = ',', dtype='int32')
+	spectral = cluster.SpectralClustering(n_clusters = 10, eigen_solver = 'arpack', affinity = "nearest_neighbors")
+	spectral.fit(features[6001:,])
+	y_pred = spectral.labels_.astype(np.int)
+
+
+	similarityMatrix = cluster.SpectralClustering(n_clusters = 10, eigen_solver = 'arpack', affinity = "precomputed")
+	similarityMatrix.fit(M)
+	y_labels = similarityMatrix.labels_.astype(np.int)
+	print y_labels
 
 
 run()
